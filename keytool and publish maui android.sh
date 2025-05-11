862dbf848d657dfa46416dfe0895b806032a16e4
Özgür - MacbookPro162019 <muzkaviy@gmail.com>
2025-05-11 15:33:33 +0300

    some fixes

--- keytool and publish maui android.sh
+++ keytool and publish maui android.sh
@@ -5,3 +5,8 @@
 dotnet publish -f net8.0-android -c Release -p:AndroidKeyStore=true -p:AndroidSigningKeyStore=ozgur-elfuertech-pilotage-calculator.keystore -p:AndroidSigningKeyAlias=ozgur-elfuertech-pilotage-calculator -p:AndroidSigningKeyPass=123456 -p:AndroidSigningStorePass=123456
 
 dotnet publish -f:net8.0-android -c Release -p:AndroidPackageFormat=aab
+
+
+# IPA dosyası oluşturma
+dotnet publish -c Release -f net8.0-ios -r ios-arm64 -p:ArchiveOnBuild=true
+# bin/release/net8.0-ios/ios-arm64/publish klasöründe oluşan ipa dosyasını Transporter Mac App ile Apple'a taşı.
